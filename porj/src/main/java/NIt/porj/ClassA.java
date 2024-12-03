d4fc07ff68f7508ef1c651ed848a790e6af42f3b
HP <HP@LAPTOP-C5AHSE4N>
2024-12-03 11:01:22 +0530

    meth1 updateed

--- porj/src/main/java/NIt/porj/ClassA.java
+++ porj/src/main/java/NIt/porj/ClassA.java
@@ -5,15 +5,15 @@ public class ClassA {
 	void meth1() {
 		
 		System.out.println("the is void method");
-		
-		
-		
+	
 		
 	}
 	
 	public static void main(String[] args) {
 		
 		ClassA a  = new ClassA();
+		a.meth1();
+		
 		a.meth1();
 	}
 }
